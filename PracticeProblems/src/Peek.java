928cd9b2a8e0de44961fe1bebc310e8ba6dd46eb
Premkumar1726 <162665646+Premkumar1726@users.noreply.github.com>
2024-04-23 12:10:52 +0530

    Array problem peek

---
+++ PracticeProblems/src/Peek.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class Main{
+
+    static int[] peek(int[] array){
+        int[] peek = new int[array.length];
+        int index = 0;
+        for(int i = 0;i < array.length;i++){
+            if(i != 0 && i != array.length-1){
+                if(array[i] > array[i-1] && array[i] > array[i+1] ){
+                    peek[index] = array[i];
+                }
+            }
+            index++;
+        }
+        return peek;
+    }
+    
+    public static void main(String[]args){
+        int[] ar= {1,3,2};
+        System.out.println(Arrays.toString(peek(ar)));
+    }
+}
