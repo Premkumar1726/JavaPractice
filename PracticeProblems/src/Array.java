5aaefb7381b7e43ce3fe2de72f7ac09650dc08e9
Premkumar1726 <162665646+Premkumar1726@users.noreply.github.com>
2024-04-27 11:03:10 +0530

    Reverse of an array problem

--- PracticeProblems/src/Array.java
+++ PracticeProblems/src/Array.java
@@ -9,4 +9,12 @@ public class Array {
         }
         return minmax;
     }
+
+    static int[] reverse(int[] array){
+        int[] reverse = new int[array.length];
+        for(int i = 0; i < array.length; i++){
+            reverse[i] = array[array.length-1-i];
+        }
+        return reverse;
+    }
 }
--- PracticeProblems/src/Main.java
+++ PracticeProblems/src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Main{
     public static void main(String[]args){
         int[]array = {7,6,9,5,1,0,2,9};
-        System.out.println("minimum in the array :"+Array.MinMax(array)[0]);
-        System.out.println("maximum in the array: "+Array.MinMax(array)[1]);
+/*        System.out.println("minimum in the array :"+Array.MinMax(array)[0]);
+        System.out.println("maximum in the array: "+Array.MinMax(array)[1]);*/
+        System.out.println("reverse of the Array: "+Arrays.toString(Array.reverse(array)));
     }
 }
